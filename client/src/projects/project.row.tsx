fa941bb2de63572b3c4abb70725d2660e3922e55
max-im <pogidaevmo@gmail.com>
2023-07-03 12:59:20 +0300

    add control styles

--- client/src/projects/project.row.tsx
+++ client/src/projects/project.row.tsx
@@ -31,15 +31,15 @@ const ProjectRow: FC<IProjectRowProps> = ({ project }) => {
 
     return (
         <Link to={projectsRoute.url + '/' + project.id}>
-            <ListItem disablePadding sx={{'&:hover .project__control': {visibility: 'visible'}}}>
-                <ListItemButton>
+            <ListItem disablePadding sx={{ '&:hover .project__control': { visibility: 'visible' }, '&:hover .project__controlWrap': { top: 7, opacity: 1 } }}>
+                <ListItemButton sx={{position: 'relative'}}>
                     <ListItemText
-                        primary={ <Typography sx={{color: 'text.primary'}}>{project.title}</Typography>}
+                        primary={ <Typography sx={{color: 'text.primary', fontSize: 18, position: 'relative', top: 10}}>{project.title}</Typography>}
                         secondary={
-                            <Box sx={{display: 'flex'}}>
+                            <Box className="project__controlWrap" sx={{display: 'flex', position: 'absolute', top: 0, transition: '0.5s', opacity: 0}}>
                                 <Box className="project__control" sx={{ visibility: 'hidden', display: 'flex'}}>
-                                    <Typography component='span' sx={{ opacity: 0.7, display: 'flex', alignItems: 'center', '&:hover': { color: 'secondary.main' } }} onClick={(e) => onDelete(e, project.id)}>Delete <DeleteIcon sx={{ml: 0.5, fontSize: 14}} /></Typography>
-                                    <Typography component='span' sx={{ ml: 2, opacity: 0.7, display: 'flex', alignItems: 'center', '&:hover': { color: 'secondary.main' } }} onClick={(e) => onSettings(e, project.id)}>Settings <SettingsIcon sx={{ml: 0.5, fontSize: 14}} /></Typography>
+                                    <Typography component='span' sx={{ fontSize: 10, opacity: 0.7, display: 'flex', alignItems: 'center', '&:hover': { color: 'secondary.main' } }} onClick={(e) => onDelete(e, project.id)}>Delete <DeleteIcon sx={{ml: 0.5, fontSize: 14}} /></Typography>
+                                    <Typography component='span' sx={{ ml: 2, fontSize: 10, opacity: 0.7, display: 'flex', alignItems: 'center', '&:hover': { color: 'secondary.main' } }} onClick={(e) => onSettings(e, project.id)}>Settings <SettingsIcon sx={{ml: 0.5, fontSize: 14}} /></Typography>
                                 </Box>
                             </Box>
                         }
