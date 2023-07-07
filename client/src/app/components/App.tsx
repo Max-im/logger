da060ccf483606a102dabcbc37325609baf1f82f
max-im <pogidaevmo@gmail.com>
2023-07-07 21:14:15 +0300

    add app styles

--- client/src/app/components/App.tsx
+++ client/src/app/components/App.tsx
@@ -1,13 +1,14 @@
 import React, { Suspense, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Container, Box } from '@mui/material';
-import Header from './header/header.component';
-import Aside from './aside/aside.component';
-import { useAppDispatch, useAppSelector } from './hooks/redux';
-import { routes } from './routes';
-import { RoleTypes } from './user/user.model';
-import Theme from './theme/theme.component';
-import { getPlansAction } from './plan';
+import Header from '../../header/header.component';
+import Aside from '../../aside/aside.component';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { routes } from '../../routes';
+import { RoleTypes } from '../../user/user.model';
+import Theme from '../../theme/theme.component';
+import { getPlansAction } from '../../plan';
+import styles from '../styles/App.module.scss';
 
 function App() {
     const dispatch = useAppDispatch();
@@ -20,25 +21,13 @@ function App() {
         dispatch(getPlansAction());
     }, [dispatch]);
 
-    const largeStyles = {
-        height: '100%', pl: '0px !important', pr: '0px !important', display: 'flex', position: 'relative',
-    };
-
-    const innterWrap = {
-        display: 'flex', flexDirection: 'column', width: '100%', pr: '0px !important', pb: '0px !important',
-    };
-
-    const appStyles = {
-        display: 'flex', flexDirection: 'column', minHeight: '100vh', p: 2,
-    };
-
     return (
         <Theme>
             <BrowserRouter>
-                <Box className="app" sx={appStyles}>
-                    <Container maxWidth="lg" sx={largeStyles}>
+                <Box className={styles.app__wrapper}>
+                    <Container maxWidth="lg" className={styles.app__container}>
                         <Aside user={user} activeRoutes={activeRoutes} />
-                        <Container sx={innterWrap}>
+                        <Container className={styles.app__main}>
                             <Header />
                             <Box sx={{ flexGrow: 1 }}>
                                 <Suspense fallback="loading">
